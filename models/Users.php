422f474f6ff8cf91eac3662a1a2419adfc43e040
Huynt95 <huyntph03066@fpt.edu.vn>
2015-05-20 20:22:30 +0700

    initial commit

---
+++ models/Users.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $age
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'name', 'age'], 'required'],
+            [['id', 'age'], 'integer'],
+            [['name'], 'string', 'max' => 30]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'age' => 'Age',
+        ];
+    }
+}
